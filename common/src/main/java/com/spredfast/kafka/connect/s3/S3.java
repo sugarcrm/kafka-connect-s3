0a47a6f0df2b973b625df79e958bac5afb405358
Jacob Gminder <jacob.gminder@sugarcrm.com>
2023-02-01 20:30:48 -0500

    CXP-2397: Create an integration test for the sink connector

--- common/src/main/java/com/spredfast/kafka/connect/s3/S3.java
+++ common/src/main/java/com/spredfast/kafka/connect/s3/S3.java
@@ -1,27 +1,34 @@
 package com.spredfast.kafka.connect.s3;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.S3ClientOptions;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import java.util.Map;
-import java.util.Objects;
 
 public class S3 {
 
 	public static AmazonS3 s3client(Map<String, String> config) {
-		// Use default credentials provider that looks in Env + Java properties + profile + instance role
-		AmazonS3 s3Client = new AmazonS3Client();
-
-		// If worker config sets explicit endpoint override (e.g. for testing) use that
-		String s3Endpoint = config.get("s3.endpoint");
-		if (s3Endpoint != null && !Objects.equals(s3Endpoint, "")) {
-			s3Client.setEndpoint(s3Endpoint);
-		}
 		boolean s3PathStyle = Boolean.parseBoolean(config.get("s3.path_style"));
-		if (s3PathStyle) {
-			s3Client.setS3ClientOptions(S3ClientOptions.builder().setPathStyleAccess(true).build());
+		String s3Endpoint = config.get("s3.endpoint");
+
+		AmazonS3 s3Client;
+		if (s3Endpoint == null || s3Endpoint.equals("")) {
+			s3Client = AmazonS3ClientBuilder
+				.standard()
+				.withPathStyleAccessEnabled(s3PathStyle)
+				.build();
+		} else {
+			// Testing with localstack
+			s3Client = AmazonS3ClientBuilder
+				.standard()
+				.withEndpointConfiguration(new EndpointConfiguration(config.get("s3.endpoint"), config.get("region")))
+				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(config.get("aws.accessKeyId"), config.get("aws.secretAccessKey"))))
+				.withPathStyleAccessEnabled(s3PathStyle)
+				.build();
 		}
+
 		return s3Client;
 	}
-
 }
